cdb1ca8650da116ac116ed90edb2856cb29dd970
xiaoyulong <xiaoyulong1988@outlook.com>
2019-01-24 14:51:55 +0800

    task\pom.xml

--- task/src/main/java/com/cehome/task/client/TimeTaskPlugin.java
+++ task/src/main/java/com/cehome/task/client/TimeTaskPlugin.java
@@ -1,22 +1,22 @@
+/*
+ *
+ */
 package com.cehome.task.client;
-
-import jsharp.util.Convert;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 
 import com.alibaba.fastjson.JSONObject;
 
 public abstract class TimeTaskPlugin {
-	protected static final Logger logger = LoggerFactory.getLogger(TimeTaskPlugin.class);
-	 
+    protected static final Logger logger = LoggerFactory.getLogger(TimeTaskPlugin.class);
 
-	public String[][] getConfigDesc() {
-		return null;
-	}
-	 
-	public abstract void run(TimeTaskContext context, JSONObject args)  throws Exception;
-	public abstract void stop(TimeTaskContext context)  throws Exception;
-	
+    public String[][] getConfigDesc() {
+	return null;
+    }
+
+    public abstract void run(TimeTaskContext context, JSONObject args) throws Exception;
+
+    public abstract void stop(TimeTaskContext context) throws Exception;
+
 }
